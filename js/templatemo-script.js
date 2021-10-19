3113559cc2e3b12e5e69c818ee17ffcfdf3b050f
sarvar9417 <69727326+sarvar9417@users.noreply.github.com>
2021-10-19 14:16:51 +0500

    x

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -4,13 +4,33 @@
 
 /* HTML document is loaded. DOM is ready.
 -----------------------------------------*/
-$(document).ready(function(){
+$(document).ready(function () {
 
 	// Mobile menu
-	$('.mobile-menu-icon').click(function(){
+	$('.mobile-menu-icon').click(function () {
 		$('.tm-nav').toggleClass('show');
 	});
-  
-  	// http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
-  	$('body').bind('touchstart', function() {});
+
+	// http://stackoverflow.com/questions/2851663/how-do-i-simulate-a-hover-with-a-touch-in-touch-enabled-browsers
+	$('body').bind('touchstart', function () { });
 });
+
+
+function sendMessage() {
+	var tempParams = {
+		name: document.getElementById("name").value,
+		phone: document.getElementById("phone").value,
+		message: document.getElementById("message").value
+	}
+
+	emailjs.send("service_x3bgq8p", "template_8a5mk77", tempParams)
+		.then(
+			() => {
+				document.getElementById("send").style.display = "block"
+			}
+	)
+	.catch(()=>{
+		document.getElementById("error").style.display = "block"
+
+	})
+}
